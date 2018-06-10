700f2efa13f79fbefe2a1d2b8610de4ea7c255c2
Alexander Topchiy <topchiyau@gmail.com>
2018-06-10 13:13:42 +0300

    Implement Preference summaries

--- app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsfeed/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.newsfeed;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -13,12 +16,34 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
 
-    public static class NewsPreferenceFragment extends PreferenceFragment {
+    public static class NewsPreferenceFragment extends PreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
 
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference keyword = findPreference(getString(R.string.settings_keyword_key));
+            bindPreferenceSummaryToValue(keyword);
+        }
+
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            // The code in this method takes care of updating the displayed preference summary after it has been changed
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences =
+                    PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
